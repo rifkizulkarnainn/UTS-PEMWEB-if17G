49ecb058d336ce40683d09b25ad146e03ef78182
rifkizulkarnainn <50473936+rifkizulkarnainn@users.noreply.github.com>
2019-05-10 11:35:32 +0700

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="style_form.css">
+<?php
+ session_start(); // memulai session
+ session_destroy(); // menghapus session
+ header("location:form_login.php"); // mengambalikan ke form_login.php
+ ?>
+ 
+ 
+ 
