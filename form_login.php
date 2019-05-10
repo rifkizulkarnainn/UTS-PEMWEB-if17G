eebbe43cbdea0fbe3c64407ccdc5bd958edea488
rifkizulkarnainn <50473936+rifkizulkarnainn@users.noreply.github.com>
2019-05-10 11:30:29 +0700

    Add files via upload

---
+++ form_login.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+        
+        <h3>Form Login Admin</h3>
+        
+			<form id="form_input" method="post" action ="proses_login.php">
+				<div class="form-group">
+					Username : 
+					<input class="form-control" type="text" name="username" required />
+				</div>
+				<div class="form-group">
+					Password : 
+					<input class="form-control" type="password" name="password" required />
+				</div>
+				<div class="form-group">
+					<input class="btn btn-danger" type="submit" value="Login">
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+
+
